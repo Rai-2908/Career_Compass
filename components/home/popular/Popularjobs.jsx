8312c361b1c2ed846d85bdc9b9107e22d9578aa9
Shivam Rai <shivamrai7509@gmail.com>
2023-07-27 19:38:55 +0530

    home sceen done

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 import { useRouter } from 'expo-router';
 import styles from './popularjobs.style';
@@ -12,7 +12,15 @@ const Popularjobs = () => {
     query: 'React developer',
     num_pages: 1
   });
+  
+  const [selectedJob, setSelectedJob] =useState()
 
+  const handleCardPress = (item) => {
+
+  }
+
+
+  
   return (
     <View style={styles.container}>
       <View style={styles.header}>
